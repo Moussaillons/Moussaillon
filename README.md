a1a7e90468c71bfb66aa45edb5f42bd31595d9d5
Louis Desportes <louis@akkes.fr>
2016-07-16 17:17:44 +0200

    implement firsts recommendations from #5

--- README.md
+++ README.md
@@ -17,6 +17,12 @@ mkvirtualenv moussaillon
 pip install -r requirements.txt
 ```
 
+to be able to test subdomains, you need to modify the hosts file accordingly to your system and add:
+
+```
+127.0.0.1   federation.dev association.federation.dev
+```
+
 ## Usage
 ### Launch local server
 ```
@@ -26,6 +32,9 @@ export FLASK_DEBUG=1
 cd moussaillon
 flask run
 ```
+
+### Test
+Open [federation.dev](federation.dev) or [association.federation.dev](association.federation.dev) in your browser
 
 ### Leave environment
 ```
--- moussaillon/moussaillon.py
+++ moussaillon/moussaillon.py
@@ -1,10 +1,51 @@
 from flask import Flask
 app = Flask(__name__)
 
+app.config['SERVER_NAME'] = 'federation.dev:5000'
+
+@app.route('/', subdomain='<association_name>')
+def website_home_route(association_name):
+    return "Welcome to "+association_name+" website"
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def root_route():
+    return "Root"
+
+@app.route('/login')
+def login_route():
+    return "Login"
+
+@app.route('/posts/')
+def posts_route():
+    return "Posts"
+
+@app.route('/posts/new')
+def new_post_route():
+    return "New post"
+
+@app.route('/posts/<string:post_name>')
+def edit_post_route(post_name):
+    return "Edit post: " + post_name
+
+@app.route('/gallery/')
+def gallery_route():
+    return "Gallery"
+
+@app.route('/gallery/upload')
+def upload_route():
+    return "Upload items"
+
+@app.route('/calendar/')
+def calendar_route():
+    return "Events"
+
+@app.route('/calendar/new')
+def new_event_route():
+    return "New event"
+
+@app.route('/calendar/<string:event_name>')
+def edit_event_route(event_name):
+    return "Edit event: " + event_name
 
 if __name__ == '__main__':
     app.run()
