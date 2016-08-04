506742b1fcd15b9ea0c1f1b84df55c9bf4c57020
Louis Desportes <louis@akkes.fr>
2016-08-04 19:36:49 +0200

    update URL according to #5 discussions

--- moussaillon/moussaillon.py
+++ moussaillon/moussaillon.py
@@ -4,59 +4,90 @@ app = Flask(__name__)
 app.config['SERVER_NAME'] = 'federation.dev:5000'
 
 
+@app.route('/')
 @app.route('/', subdomain='<association_name>')
-def website_home_route(association_name):
+def website_home_route(association_name=None):
     return "Welcome to "+association_name+" website"
 
 
-@app.route('/')
-def root_route():
-    return render_template('panel/text.html', title="Dashboard", content="Bienvenue!")
+@app.route('/panel/')
+@app.route('/panel/', subdomain='<association_name>')
+def root_route(association_name=None):
+    return render_template('panel/text.html',
+                           title="Dashboard",
+                           content="Bienvenue!")
 
 
-@app.route('/login')
-def login_route():
-    return render_template('panel/text.html', title="Login", content="not implemented yet")
+@app.route('/panel/login')
+@app.route('/panel/login', subdomain='<association_name>')
+def login_route(association_name=None):
+    return render_template('panel/text.html',
+                           title="Login",
+                           content="not implemented yet")
 
 
-@app.route('/posts/')
-def posts_route():
-    return render_template('panel/text.html', title="Posts", content="not implemented yet")
+@app.route('/panel/posts/')
+@app.route('/panel/posts/', subdomain='<association_name>')
+def posts_route(association_name=None):
+    return render_template('panel/text.html',
+                           title="Posts",
+                           content="not implemented yet")
 
 
-@app.route('/posts/new')
-def new_post_route():
-    return render_template('panel/text.html', title="New Post", content="not implemented yet")
+@app.route('/panel/posts/new')
+@app.route('/panel/posts/new', subdomain='<association_name>')
+def new_post_route(association_name=None):
+    return render_template('panel/text.html',
+                           title="New Post",
+                           content="not implemented yet")
 
 
-@app.route('/posts/<string:post_name>')
-def edit_post_route(post_name):
-    return render_template('panel/text.html', title="Edit post: " + post_name, content="not implemented yet")
+@app.route('/panel/posts/<string:post_name>')
+@app.route('/panel/posts/<string:post_name>', subdomain='<association_name>')
+def edit_post_route(post_name, association_name=None):
+    return render_template('panel/text.html',
+                           title="Edit post: " + post_name,
+                           content="not implemented yet")
 
 
-@app.route('/gallery/')
-def gallery_route():
-    return render_template('panel/text.html', title="Gallery", content="not implemented yet")
+@app.route('/panel/gallery/')
+@app.route('/panel/gallery/', subdomain='<association_name>')
+def gallery_route(association_name=None):
+    return render_template('panel/text.html',
+                           title="Gallery",
+                           content="not implemented yet")
 
 
-@app.route('/gallery/upload')
-def upload_route():
-    return render_template('panel/text.html', title="Upload items", content="not implemented yet")
+@app.route('/panel/gallery/upload')
+@app.route('/panel/gallery/upload', subdomain='<association_name>')
+def upload_route(association_name=None):
+    return render_template('panel/text.html',
+                           title="Upload items",
+                           content="not implemented yet")
 
 
-@app.route('/calendar/')
-def calendar_route():
-    return render_template('panel/text.html', title="Events", content="not implemented yet")
+@app.route('/panel/calendar/')
+@app.route('/panel/calendar/', subdomain='<association_name>')
+def calendar_route(association_name=None):
+    return render_template('panel/text.html',
+                           title="Events",
+                           content="not implemented yet")
 
 
-@app.route('/calendar/new')
-def new_event_route():
-    return render_template('panel/text.html', title="New event", content="not implemented yet")
+@app.route('/panel/calendar/new')
+@app.route('/panel/calendar/new', subdomain='<association_name>')
+def new_event_route(association_name=None):
+    return render_template('panel/text.html',
+                           title="New event",
+                           content="not implemented yet")
 
 
-@app.route('/calendar/<string:event_name>')
-def edit_event_route(event_name):
-    return render_template('panel/text.html', title="Edit event: " + event_name, content="not implemented yet")
+@app.route('/panel/calendar/<string:event_name>')
+@app.route('/panel/calendar/<string:event_name>', subdomain='<association_name>')
+def edit_event_route(event_name, association_name=None):
+    return render_template('panel/text.html',
+                           title="Edit event: " + event_name,
+                           content="not implemented yet")
 
 
 if __name__ == '__main__':
